1e7c0644dd64d86e69f5872479cde2f93938338c
Morimoto Keigo <keigo7624@gmail.com>
2018-09-08 14:41:16 +0900

    add Scene

---
+++ Akatsuki/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	private void Awake()
+	{
+		if(instance == null){
+			instance = this;
+		}else if(instance != this){
+			Destroy(gameObject);
+		}
+
+		InitGame();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void InitGame(){
+
+	}
+}
