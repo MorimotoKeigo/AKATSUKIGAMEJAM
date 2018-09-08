aa80b0b5e3756d1b36b92223a0f4c762cc130d32
Morimoto Keigo <keigo7624@gmail.com>
2018-09-08 16:43:21 +0900

    GameManager

--- Akatsuki/Assets/GameManager.cs
+++ Akatsuki/Assets/GameManager.cs
@@ -6,6 +6,13 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;
 
+
+	//public
+	public int PlayerHP;
+	public int LightCount;
+
+	public int EnemyAttckDamege;
+
 	private void Awake()
 	{
 		if(instance == null){
@@ -28,6 +35,28 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void InitGame(){
+		PlayerHP = 100;
+		LightCount = 3;
+	}
+		
+	void EnemyAttack(){
+		PlayerHP -= 10;
+	}
 
+	int LightNum(){
+		return LightCount;	
+	}
+
+	void AddLight(){
+
+		if (LightCount < 3) {
+			LightCount++;
+		}
+	}
+
+	void SubLight(){
+		if (LightCount > 0) {
+			LightCount--;
+		}
 	}
 }
