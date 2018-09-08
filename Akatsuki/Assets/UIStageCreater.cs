cf63713ac2cfe96eac40523ec4bb67aaf838488d
akio-okumura <oku.develop@gmail.com>
2018-09-08 20:58:40 +0900

    oku_0908作業終了コミット

--- Akatsuki/Assets/UIStageCreater.cs
+++ Akatsuki/Assets/UIStageCreater.cs
@@ -11,16 +11,9 @@ public class UIStageCreater:MonoBehaviour{
 	//配置するオブジェクト
 	public GameObject floor_UI;
 	public GameObject wall_UI;
-
-
 	public GameObject Map_parent;
-	//public GameObject player;
 
-
-	//public Vector3 spaceScale = new Vector3(1.0f,1.0f,0f);
-
-	//char[,] StageData = new char[10,10];
-	//List<GameObject> StageList = new List<GameObject>();
+	private int num = 0; // 何マス目か判別するint
 
 	void Start () {
 
@@ -30,20 +23,11 @@ public class UIStageCreater:MonoBehaviour{
 			GameObject.DestroyImmediate(stage);
 		}
 
-		// var createPos = new Vector3 (-185, 170, 0);
 		UI_CreateStage();
 	}
 
 
 	void UI_CreateStage(){
-		/*
-		pos.x += 5.0f;
-		pos.z -= 5.0f;
-
-		pos.y += 2.5f;
-		*/
-
-		// Vector3 originPos = pos;
 		string stageTextData = textAsset.text;
 
 		foreach(char c in stageTextData){
@@ -52,34 +36,15 @@ public class UIStageCreater:MonoBehaviour{
 
 			if(c == 'F'){
 				obj = Instantiate(floor_UI, Map_parent.transform) as GameObject;
-				obj.name = "UI_Floor";
+				obj.name = "UI_floor" + num;
 				obj.tag = "UI_Stage";
-				// obj.transform.parent = Map_parent.transform;
-				// pos.x += 10.0f;
+				num++;
 			} else if (c == '#') { // else(c == '#')
 				obj = Instantiate(wall_UI, Map_parent.transform) as GameObject;
-				obj.name = "UI_Wall";
+				obj.name = "UI_wall" + num;
 				obj.tag = "UI_Stage";
-				// obj.GetComponent <RectTransform>();
-				// obj.transform.parent = Map_parent.transform;
-				// pos.x += 10.0f;
+				num++;
 			}
-			/*else if(c == 'T'){
-				//obj = Instantiate(tree, pos, Quaternion.Euler(0, 0, 0)) as GameObject;
-				//obj.name = "Tree";
-				//obj.tag = "Stage";
-				//pos.x += 10.0f;
-			}
-			*/
-			/*
-			else if(c == '\n'){
-				// pos.z -= 10.0f;
-				// pos.x = originPos.x;
-			}else if(c == ' '){
-				// pos.x += 10.0f;
-			}
-			*/
-
 		}
 	}
 
