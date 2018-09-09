5eb16b769530df2c5be96396615e3d02225cc11c
nuekodori <43056189+nuekodori@users.noreply.github.com>
2018-09-09 11:14:25 +0900

    宝箱！

---
+++ Akatsuki/Assets/treasure/Treasure.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour {
+	GameObject Player;
+	public float distance_PT=200f;
+
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance_PT=Vector3.Distance(this.gameObject.transform.position,Player.transform.position);
+
+		if (distance_PT < 5.0f) {
+			Get_Treasure ();
+		}
+	}
+
+
+	void Get_Treasure(){
+
+
+		//ここにGameEngineを呼び出す
+		Destroy (this.gameObject);
+
+	}
+
+
+}
